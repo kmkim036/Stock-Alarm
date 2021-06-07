20d3f18b3750c2637ea26929a18e1296e6d10b6a
kmkim036 <kmkim036@gmail.com>
2021-06-07 15:24:59 +0900

    Fix: organize into class

--- main.py
+++ main.py
@@ -4,62 +4,66 @@ import openpyxl
 import datetime
 
 
-def search(num):
-    if num == 1:
-        itemcode = "005930"
-    elif num == 2:
-        itemcode = "035720"
-    elif num == 3:
-        itemcode = "000660"
-    elif num == 4:
-        itemcode = "035420"
+class enterprise:
+    def __init__(self, name, itemcode):
+        self.name = name
+        self.itemcode = itemcode
 
-    host = 'http://api.finance.naver.com'
-    path = '/service/itemSummary.nhn'
-    param = {'itemcode': itemcode}
-    url = host + path
+    def search(self):
+        host = 'http://api.finance.naver.com'
+        path = '/service/itemSummary.nhn'
+        param = {'itemcode': self.itemcode}
+        url = host + path
+        res = requests.get(url, params=param)
+        if res.status_code != 200:
+            print("ERROR:" + "API Get Failed in " + self.name)
+            return
+        data = res.json()
+        self.save(data)
 
-    res = requests.get(url, params=param)
-    if res.status_code != 200:
-        print("API Get Failed")
-        quit()
+    def save(self, data):
+        dt_now = datetime.datetime.now()
 
-    data = res.json()
-    save(data, num)
+        risefall = data["risefall"]
+        if risefall == 1:
+            rf = "상한"
+        elif risefall == 2:
+            rf = "상승"
+        elif risefall == 3:
+            rf = "보합"
+        elif risefall == 4:
+            rf = "하한"
+        else:
+            rf = "하락"
 
+        wb = openpyxl.load_workbook(
+            '/home/kmkim/Projects/git/kmkim036/Stock-Manage/data.xlsx')
 
-def save(data, itemcode):
-    dt_now = datetime.datetime.now()
+        ws = wb[self.name]
 
-    risefall = data["risefall"]
-    if risefall == 1:
-        rf = "상한"
-    elif risefall == 2:
-        rf = "상승"
-    elif risefall == 3:
-        rf = "보합"
-    elif risefall == 4:
-        rf = "하한"
-    else:
-        rf = "하락"
+        A_lastrow = 'A' + str(ws.max_row)
 
-    wb = openpyxl.load_workbook(
-        '/home/kmkim/Projects/git/kmkim036/Stock-Manage/data.xlsx')
+        if dt_now.date() < ws[A_lastrow].value.date():
+            print("ERROR: "+ "Date overflow in " + self.name)
+            return
 
-    if itemcode == 1:
-        ws = wb['samsung']
-    elif itemcode == 2:
-        ws = wb['kakao']
-    elif itemcode == 3:
-        ws = wb['SKhynix']
-    else:
-        ws = wb['naver']
+        if dt_now.date() == ws[A_lastrow].value.date():
+            ws.delete_rows(ws.max_row)
 
-    ws.append([dt_now.date(), data["marketSum"], data["per"], data["eps"], data["pbr"], data["now"],
-              data["diff"], data["rate"], data["quant"], data["amount"], data["high"], data["low"], rf])
-    wb.save('/home/kmkim/Projects/git/kmkim036/Stock-Manage/data.xlsx')
-    wb.close()
+        ws.append([dt_now.date(), data["marketSum"], data["per"], data["eps"], data["pbr"], data["now"],
+                   data["diff"], data["rate"], data["quant"], data["amount"], data["high"], data["low"], rf])
+        wb.save('/home/kmkim/Projects/git/kmkim036/Stock-Manage/data.xlsx')
+        wb.close()
 
 
-for i in range(1, 5):
-    search(i)
+e1 = enterprise("samsung", "005930")
+e1.search()
+
+e2 = enterprise("kakao", "035720")
+e2.search()
+
+e3 = enterprise("SKhynix", "000660")
+e3.search()
+
+e4 = enterprise("naver", "035420")
+e4.search()
