a6297cd57fd078a6d056433df56e275555a599a8
kmkim036 <kmkim036@gmail.com>
2021-06-12 23:53:23 +0900

    Add: record log

---
+++ src/log.py
@@ -0,0 +1,33 @@
+import datetime
+
+
+def record_kakao_send(ent_name):
+    try:
+        f = open(
+            '/home/kmkim/Projects/git/kmkim036/Stock-Manage/log/kakao_send.log', 'a')
+    except FileNotFoundError:
+        return
+
+    f.write(str(datetime.datetime.now()) + ": " + ent_name + "\n")
+    f.close()
+
+
+def record_error(code, status_code, func):
+    try:
+        f = open('/home/kmkim/Projects/git/kmkim036/Stock-Manage/log/error.log', 'a')
+    except FileNotFoundError:
+        return
+
+    if code == 1:
+        msg = "Date overflow in "
+    elif code == 2:
+        msg = "File open error in "
+    elif code == 3:
+        msg = str(status_code) + ": Post API failed in "
+    elif code == 4:
+        msg = str(status_code) + ": Get API failed in "
+    elif code == 5:
+        msg = "Access token expired in "
+
+    f.write(str(datetime.datetime.now()) + ": " + msg + func + "\n")
+    f.close()
