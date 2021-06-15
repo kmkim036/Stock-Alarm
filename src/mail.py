d55e722eefb179f7a16e58dad24ec9637ef8a13e
kmkim036 <kmkim036@gmail.com>
2021-06-15 23:21:13 +0900

    rm comments & add sys module

--- src/mail.py
+++ src/mail.py
@@ -1,3 +1,4 @@
+import sys
 import json
 import smtplib
 from email.mime.text import MIMEText
@@ -31,8 +32,8 @@ def send_mail(ent_name):
     msg['From'] = sendEmail
     msg['To'] = recvEmail
 
-    s = smtplib.SMTP(smtpName, smtpPort)  # 메일 서버 연결
-    s.starttls()  # TLS 보안 처리
-    s.login(sendEmail, password)  # 로그인
-    s.sendmail(sendEmail, recvEmail, msg.as_string())  # 메일 전송, 문자열로 변환하여 보냅니다.
-    s.close()  # smtp 서버 연결을 종료합니다.
+    s = smtplib.SMTP(smtpName, smtpPort)
+    s.starttls()
+    s.login(sendEmail, password)
+    s.sendmail(sendEmail, recvEmail, msg.as_string())  
+    s.close()  
