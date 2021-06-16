d8f6dd89e7abc85ed7c9ff45f1a5a5cc0ad2fda7
kmkim036 <kmkim036@gmail.com>
2021-06-16 10:11:23 +0900

    Add: script file for auto-running

---
+++ script/auto.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+python3 /home/kmkim/Projects/git/kmkim036/Stock-Alarm/src/main.py
+
+sleep 5
+
+rm -r /home/kmkim/Projects/git/kmkim036/Stock-Alarm/src/__pycache__
+
+git add .
+
+git commit -am "Update: $(date +"%Y-%m-%d")"
+
+git push
