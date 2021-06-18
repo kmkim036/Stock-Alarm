522810f2018657a0beef4fb410acdd638a63783c
kmkim036 <kmkim036@gmail.com>
2021-06-19 00:51:29 +0900

    Subdivide: error log

--- src/kakao_api.py
+++ src/kakao_api.py
@@ -8,9 +8,9 @@ import log
 security_FILE_PATH = '/home/kmkim/Projects/security.json'
 
 
-def check_refresk_token(json):
+def check_word(json, word):
     try:
-        buf = json['refresh_token']
+        buf = json[word]
     except KeyError:
         return False
     return True
@@ -21,8 +21,9 @@ def get_token():
         with open(security_FILE_PATH) as json_file:
             json_data = json.load(json_file)
     except FileNotFoundError:
-        log.record_error(2, 0, sys._getframe().f_code.co_name)
-        return
+        msg = "File open error"
+        log.record_error(msg, sys._getframe().f_code.co_name)
+        return -1
 
     host = "https://kauth.kakao.com"
     path = "/oauth/token"
@@ -40,11 +41,18 @@ def get_token():
     }
 
     res = requests.post(url, params=parameter)
-    if res.status_code != 200:
-        log.record_error(3, res.status_code, sys._getframe().f_code.co_name)
-        return -1
-
     data = res.json()
+
+    if res.status_code != 200:
+        msg = "API POST failed, status code: " + str(res.status_code)
+        if check_word(data, 'error_code') == True:
+            msg = msg + ", error code: " + data['error_code']
+        if check_word(data, 'error_description') == True:
+            msg = msg + ", error desc: " + data['error_description']
+        if check_word(data, 'msg') == True:
+            msg = msg + ", error msg: " + data['msg']
+        log.record_error(msg, sys._getframe().f_code.co_name)
+        return -2
 
     json_data['kakao']['access-token'] = data['access_token']
     json_data['kakao']['refresh-token'] = data['refresh_token']
@@ -53,8 +61,11 @@ def get_token():
         with open(security_FILE_PATH, 'w', encoding='utf-8') as make_file:
             json.dump(json_data, make_file, indent="\t")
     except FileNotFoundError:
-        log.record_error(2, 0, sys._getframe().f_code.co_name)
-        return
+        msg = "File open error"
+        log.record_error(msg, sys._getframe().f_code.co_name)
+        return -3
+
+    return 0
 
 
 def refresh_token():
@@ -62,8 +73,9 @@ def refresh_token():
         with open(security_FILE_PATH) as json_file:
             json_data = json.load(json_file)
     except FileNotFoundError:
-        log.record_error(2, 0, sys._getframe().f_code.co_name)
-        return
+        msg = "File open error"
+        log.record_error(msg, sys._getframe().f_code.co_name)
+        return -1
 
     host = "https://kauth.kakao.com"
     path = "/oauth/token"
@@ -79,40 +91,30 @@ def refresh_token():
     }
 
     res = requests.post(url, params=parameter)
-    if res.status_code != 200:
-        log.record_error(3, res.status_code, sys._getframe().f_code.co_name)
-        return -1
-
     data = res.json()
 
+    if res.status_code != 200:
+        msg = "API POST failed, status code: " + str(res.status_code)
+        if check_word(data, 'error_code') == True:
+            msg = msg + ", error code: " + data['error_code']
+        if check_word(data, 'error_description') == True:
+            msg = msg + ", error desc: " + data['error_description']
+        if check_word(data, 'msg') == True:
+            msg = msg + ", error msg: " + data['msg']
+        log.record_error(msg, sys._getframe().f_code.co_name)
+        return -2
+
     json_data['kakao']['access-token'] = data['access_token']
-    if check_refresk_token == True:
+    if check_word(data, 'refresh_token') == True:
         json_data['kakao']['refresh-token'] = data['refresh_token']
 
     try:
         with open(security_FILE_PATH, 'w', encoding='utf-8') as make_file:
             json.dump(json_data, make_file, indent="\t")
     except FileNotFoundError:
-        log.record_error(2, 0, sys._getframe().f_code.co_name)
-        return
-
-
-def check_access_token(access_token):
-    host = "https://kapi.kakao.com"
-    path = "/v1/user/access_token_info"
-    url = host + path
-    header = {"Authorization": 'Bearer ' + access_token}
-
-    res = requests.get(url, headers=header)
-    if res.status_code != 200:
-        log.record_error(4, res.status_code, sys._getframe().f_code.co_name)
-        return -1
-
-    data = res.json()
-
-    if(data["expires_in"] < 1):
-        log.record_error(5, 0, sys._getframe().f_code.co_name)
-        return -2
+        msg = "File open error"
+        log.record_error(msg, sys._getframe().f_code.co_name)
+        return -3
 
     return 0
 
@@ -125,11 +127,9 @@ def send_to_me(ent_name):
             json_data = json.load(json_file)
     except FileNotFoundError:
         log.record_error(2, 0, sys._getframe().f_code.co_name)
-        return
+        return -1
 
     ACCESS_TOKEN = json_data["kakao"]["access-token"]
-    if check_access_token(ACCESS_TOKEN) != 0:
-        return
 
     host = "https://kapi.kakao.com"
     path = "/v2/api/talk/memo/default/send"
@@ -145,7 +145,17 @@ def send_to_me(ent_name):
     body_json = {"template_object": json.dumps(body)}
 
     res = requests.post(url, headers=header, data=body_json)
-    if res.status_code != 200:
-        log.record_error(3, res.status_code, sys._getframe().f_code.co_name)
+    data = res.json()
 
-    return
+    if res.status_code != 200:
+        msg = "API POST failed, status code: " + str(res.status_code)
+        if check_word(data, 'error_code') == True:
+            msg = msg + ", error code: " + data['error_code']
+        if check_word(data, 'error_description') == True:
+            msg = msg + ", error desc: " + data['error_description']
+        if check_word(data, 'msg') == True:
+            msg = msg + ", error msg: " + data['msg']
+        log.record_error(msg, sys._getframe().f_code.co_name)
+        return -2
+
+    return 0
